5bd85a3d5e02aadd4ba1e97dd5f452b8e7feea9d
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-06-06 10:36:05 -0400

    servie error

--- weather.js
+++ weather.js
@@ -1,36 +1,58 @@
+const http = require('http');
 const https = require('https');
 const api = require('./api.json');
 
+
 function printWeather(weather) {
-    const message = `Temperature in ${weather.location.city} is currently ${weather.current_observation.temp_f}F`;
+    const message = `The temperature in ${weather.location.city} is currently ${weather.current_observation.temp_f}F`;
     console.log(message);
 }
+
 
 function printError(error) {
     console.error(error.message);
 }
 
 function get(query) {
+   
     const readableQuery = query.replace('_', ' ');
     try {
-        const request = https.get(`https://api.wunderground.com/api/${api.key}/geolookup/conditions/q/${query}.json`, 
-        response => {
-                if(response.statusCode === 200) {
+        const request = https.get(`https://api.wunderground.com/api/${api.key}/geolookup/conditions/q/${query}.json`, response => {
+            if (response.statusCode === 200) {
                 let body = "";
+         
                 response.on('data', chunk => {
                     body += chunk;
                 });
                 response.on('end', () => {
-                    const weather = JSON.parse(body);
-                    printWeather(weather);
-                });
-                } else {
-                    const queryError = new Error(`The location "${readableQuery}" was not found.`);
-                }
-        });
-        
-    } catch(error) {
+                    try {
+                        
+                        const weather = JSON.parse(body);
 
+                        if (weather.location) {
+                     
+                            printWeather(weather);
+                        } else {
+                            const queryError = new Error(`The location "${readableQuery}" was not found.`);
+                            printError(queryError);
+                        }
+                    } catch (error) {
+                        // Parse Error
+                        printError(error);
+                    }
+                });
+            } else {
+                // Status Code Error
+                const statusCodeError = new Error(`There was an error fetching your message for ${readableQuery}. (${http.STATUS_CODES[response.statusCode]})`);
+                printError(statusCodeError);
+            }
+
+        });
+
+        request.on("error", printError);
+    } catch (error) {
+        //Malformed URL Error
+        printError(error);
     }
 }
 
