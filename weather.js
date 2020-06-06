d1e00f1db761c16e30de3d1c381db1c85c9f43da
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-06-05 22:35:39 -0400

    trying to understand try catch

--- weather.js
+++ weather.js
@@ -1,17 +1,37 @@
 const https = require('https');
 const api = require('./api.json');
 
+function printWeather(weather) {
+    const message = `Temperature in ${weather.location.city} is currently ${weather.current_observation.temp_f}F`;
+    console.log(message);
+}
+
+function printError(error) {
+    console.error(error.message);
+}
+
 function get(query) {
-    const request = https.get(`https://api.wunderground.com/api/${api.key}/geolookup/conditions/q/${query}.json`, 
-    response => {
-        let body = "";
-        response.on('data', chunk => {
-            body += chunk;
+    const readableQuery = query.replace('_', ' ');
+    try {
+        const request = https.get(`https://api.wunderground.com/api/${api.key}/geolookup/conditions/q/${query}.json`, 
+        response => {
+                if(response.statusCode === 200) {
+                let body = "";
+                response.on('data', chunk => {
+                    body += chunk;
+                });
+                response.on('end', () => {
+                    const weather = JSON.parse(body);
+                    printWeather(weather);
+                });
+                } else {
+                    const queryError = new Error(`The location "${readableQuery}" was not found.`);
+                }
         });
-        response.on('end', () => {
-            console.log(body);
-        });
-    });
+        
+    } catch(error) {
+
+    }
 }
 
 module.exports.get = get;
